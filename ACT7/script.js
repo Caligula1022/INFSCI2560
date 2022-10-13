75980da55f58e4a9730d9b0bf6f0415c0198199e
Yifei Tai <89871210+Caligula1022@users.noreply.github.com>
2022-10-13 17:18:28 -0400

    Add files via upload

---
+++ ACT7/script.js
@@ -0,0 +1,13 @@
+//this is a helper method to create a DOM node
+function createNode(element) {
+      return document.createElement(element);
+  }
+
+  function append(parent, element) {
+    return parent.appendChild(element);
+  }
+
+const ul = document.getElementById('people');
+
+//insert your code here to complete this activity
+//feel free to use the helper code above ... or take a different approach
